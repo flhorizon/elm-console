382d4e88b1a8e904eeaa34626ca7b2e0de8d5eab
Florian Fouratier <ffourati@student.42.fr>
2017-12-17 01:12:14 +0100

    Elm 0.18 syntax.

--- src/Console/Core.elm
+++ src/Console/Core.elm
@@ -1,126 +1,232 @@
-module Console.Core where
+module Console.Core exposing (..)
 
 import String
 
+
 -- IO Actions
-{-| Print a character to stdout -}
+
+
+{-| Print a character to stdout
+-}
 putChar : Char -> IO ()
-putChar c = Impure (PutS (String.cons c "") (\_ -> Pure ()))
+putChar c =
+    Impure (PutS (String.cons c "") (\_ -> Pure ()))
 
-{-| Read a character from stdin -}
+
+{-| Read a character from stdin
+-}
 getChar : IO Char
-getChar = Impure (GetC Pure)
+getChar =
+    Impure (GetC Pure)
 
-{-| Exit the program with the given exit code. -}
+
+{-| Exit the program with the given exit code.
+-}
 exit : Int -> IO ()
-exit = Impure << Exit
+exit =
+    Impure << Exit
 
-{-| Print a string to stdout. -}
+
+{-| Print a string to stdout.
+-}
 putStr : String -> IO ()
-putStr s = Impure (PutS s (\_ -> Pure ()))
+putStr s =
+    Impure (PutS s (\_ -> Pure ()))
 
-{-| Print a string to stdout, followed by a newline. -}
+
+{-| Print a string to stdout, followed by a newline.
+-}
 putStrLn : String -> IO ()
-putStrLn s = putStr s >>> putChar '\n'
+putStrLn s =
+    putStr s >>> putChar '\n'
 
-{-| Read characters from stdin until one matches the given character. -}
+
+{-| Read characters from stdin until one matches the given character.
+-}
 readUntil : Char -> IO String
-readUntil end = let go s = getChar >>= \c ->
-                           if c == end
-                           then pure s
-                           else go (String.append s (String.cons c ""))
-                in go ""
+readUntil end =
+    let
+        go s =
+            getChar
+                >>= \c ->
+                        if c == end then
+                            pure s
+                        else
+                            go (String.append s (String.cons c ""))
+    in
+        go ""
 
-{-| Write content to a file -}
+
+{-| Write content to a file
+-}
 writeFile : { file : String, content : String } -> IO ()
-writeFile obj = Impure (WriteF obj (\_ -> Pure ()))
+writeFile obj =
+    Impure (WriteF obj (\_ -> Pure ()))
 
-{-| Read a line from stdin -}
+
+{-| Read a line from stdin
+-}
 getLine : IO String
-getLine = readUntil '\n'
+getLine =
+    readUntil '\n'
+
+
 
 -- | IO Combinators
 
-{-| Apply a pure function to an IO value -}
-map : (a -> b) -> IO a -> IO b
-map f io = case io of
-  Pure   a   -> Pure (f a)
-  Impure iof -> Impure (mapF (map f) iof)
 
-{-| Apply a pure function to two IO values -}
+{-| Apply a pure function to an IO value
+-}
+map : (a -> b) -> IO a -> IO b
+map f io =
+    case io of
+        Pure a ->
+            Pure (f a)
+
+        Impure iof ->
+            Impure (mapF (map f) iof)
+
+
+{-| Apply a pure function to two IO values
+-}
 map2 : (a -> b -> result) -> IO a -> IO b -> IO result
 map2 f a b =
-  a `andThen` \x ->
-  b `andThen` \y ->
-    pure (f x y)
+    a
+        `andThen`
+            \x ->
+                b
+                    `andThen`
+                        \y ->
+                            pure (f x y)
 
-{-| Alternative interface to forEach  -}
+
+{-| Alternative interface to forEach
+-}
 mapIO : (a -> IO ()) -> List a -> IO ()
-mapIO f xs = List.foldr (seq << f) (pure ()) xs
+mapIO f xs =
+    List.foldr (seq << f) (pure ()) xs
 
-{-| Run an IO computation for each element of a list -}
+
+{-| Run an IO computation for each element of a list
+-}
 forEach : List a -> (a -> IO ()) -> IO ()
-forEach xs f = mapIO f xs
+forEach xs f =
+    mapIO f xs
 
-{-| Use a pure value where an IO computation is expected. -}
+
+{-| Use a pure value where an IO computation is expected.
+-}
 pure : a -> IO a
-pure = Pure
+pure =
+    Pure
 
-{-| Apply an IO function to an IO value -}
+
+{-| Apply an IO function to an IO value
+-}
 apply : IO (a -> b) -> IO a -> IO b
-apply iof iom = iof >>= \f ->
-                iom >>= \m ->
-                pure (f m)
+apply iof iom =
+    iof
+        >>= \f ->
+                iom
+                    >>= \m ->
+                            pure (f m)
 
-{-| Convenient operator for apply, similar to ~ in the Signal module -}
+
+{-| Convenient operator for apply, similar to ~ in the Signal module
+-}
 (<*>) : IO (a -> b) -> IO a -> IO b
-(<*>) = apply
+(<*>) =
+    apply
 
-{-| Chain together IO computations -}
+
+{-| Chain together IO computations
+-}
 andThen : IO a -> (a -> IO b) -> IO b
-andThen io f = case io of
-  Pure x     -> f x
-  Impure iof -> Impure (mapF (flip andThen f) iof)
+andThen io f =
+    case io of
+        Pure x ->
+            f x
 
-{-| Operator version of andThen -}
+        Impure iof ->
+            Impure (mapF (flip andThen f) iof)
+
+
+{-| Operator version of andThen
+-}
 (>>=) : IO a -> (a -> IO b) -> IO b
-(>>=) = andThen
+(>>=) =
+    andThen
 
-{-| Run one computation and then another, ignoring the first's output -}
+
+{-| Run one computation and then another, ignoring the first's output
+-}
 seq : IO a -> IO b -> IO b
-seq x y = x >>= \_ -> y
+seq x y =
+    x >>= \_ -> y
 
-{-| Run several computations in a sequence, combining all results into a list -}
+
+{-| Run several computations in a sequence, combining all results into a list
+-}
 sequenceMany : List (IO a) -> IO (List a)
 sequenceMany ios =
-  case ios of
-    [] -> pure []
-    first :: rest -> map2 (::) first (sequenceMany rest)
+    case ios of
+        [] ->
+            pure []
 
-{-| Operator version of seq -}
+        first :: rest ->
+            map2 (::) first (sequenceMany rest)
+
+
+{-| Operator version of seq
+-}
 (>>>) : IO a -> IO b -> IO b
-(>>>) = seq
+(>>>) =
+    seq
+
+
 
 -- Has to be >>= not >>> because of strictness!
-{-| Run the same computation over and over again forever. -}
-forever : IO a -> IO ()
-forever m = m >>= (\_ -> forever m)
 
-{-| IMPLEMENTATION DETAILS!!! -}
-type IOF a = PutS String (() -> a)    -- ^ the a is the next computation
-           | GetC (Char -> a) -- ^ the (Char -> a) is the continuation
-           | Exit Int
-           | WriteF { file : String, content : String} (() -> a)
+
+{-| Run the same computation over and over again forever.
+-}
+forever : IO a -> IO ()
+forever m =
+    m >>= (\_ -> forever m)
+
+
+{-| IMPLEMENTATION DETAILS!!!
+-}
+type IOF a
+    = PutS String (() -> a) -- ^ the a is the next computation
+    | GetC (Char -> a) -- ^ the (Char -> a) is the continuation
+    | Exit Int
+    | WriteF { file : String, content : String } (() -> a)
+
+
 
 -- The type of I/O computations.
+
+
 {-| An `IO a` is a computation that does some I/O and eventually
-    returns an `a` -}
-type IO a = Pure a
-          | Impure (IOF (IO a))
+returns an `a`
+-}
+type IO a
+    = Pure a
+    | Impure (IOF (IO a))
+
 
 mapF : (a -> b) -> IOF a -> IOF b
-mapF f iof = case iof of
-  PutS p k -> PutS p (f << k)
-  GetC k   -> GetC (f << k)
-  Exit n   -> Exit n
-  WriteF obj k -> WriteF obj (f << k)
+mapF f iof =
+    case iof of
+        PutS p k ->
+            PutS p (f << k)
+
+        GetC k ->
+            GetC (f << k)
+
+        Exit n ->
+            Exit n
+
+        WriteF obj k ->
+            WriteF obj (f << k)
--- src/Console/NativeCom.elm
+++ src/Console/NativeCom.elm
@@ -1,24 +1,30 @@
-module Console.NativeCom where
+module Console.NativeCom exposing (..)
 
 import Task exposing (Task)
-
 import Native.Console.NativeCom
 
-type IRequest = Put String
-              | Exit Int
-              | Get
-              | WriteFile { file : String, content : String }
-              | Init
 
-type alias IResponse = Maybe String
+type IRequest
+    = Put String
+    | Exit Int
+    | Get
+    | WriteFile { file : String, content : String }
+    | Init
+
+
+type alias IResponse =
+    Maybe String
+
 
 sendRequests : Signal (List IRequest) -> Signal (Task x ())
 sendRequests requests =
     Signal.map sendRequestBatch requests
 
+
 sendRequestBatch : List IRequest -> Task x ()
 sendRequestBatch requests =
     Native.Console.NativeCom.sendRequestBatch requests
+
 
 responses : Signal IResponse
 responses =
